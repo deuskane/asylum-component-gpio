a771b6d1b44359c6654d332a4f983ce56948e747
deuskane <deuskane@gmail.com>
2025-05-14 16:07:07 +0200

    Update generated csr

--- src/csr/GPIO_csr.vhd
+++ src/csr/GPIO_csr.vhd
@@ -45,6 +45,9 @@ architecture rtl of GPIO_registers is
 
   signal   sig_busy  : std_logic;
 
+  constant INIT_data : std_logic_vector(8-1 downto 0) :=
+             "00000000" -- value
+           ;
   signal   data_wcs       : std_logic;
   signal   data_we        : std_logic;
   signal   data_wdata     : std_logic_vector(8-1 downto 0);
@@ -59,6 +62,9 @@ architecture rtl of GPIO_registers is
   signal   data_rdata_hw  : std_logic_vector(8-1 downto 0);
   signal   data_rbusy     : std_logic;
 
+  constant INIT_data_oe : std_logic_vector(8-1 downto 0) :=
+             "00000000" -- value
+           ;
   signal   data_oe_wcs       : std_logic;
   signal   data_oe_we        : std_logic;
   signal   data_oe_wdata     : std_logic_vector(8-1 downto 0);
@@ -73,6 +79,9 @@ architecture rtl of GPIO_registers is
   signal   data_oe_rdata_hw  : std_logic_vector(8-1 downto 0);
   signal   data_oe_rbusy     : std_logic;
 
+  constant INIT_data_in : std_logic_vector(8-1 downto 0) :=
+             "00000000" -- value
+           ;
   signal   data_in_wcs       : std_logic;
   signal   data_in_we        : std_logic;
   signal   data_in_wdata     : std_logic_vector(8-1 downto 0);
@@ -87,6 +96,9 @@ architecture rtl of GPIO_registers is
   signal   data_in_rdata_hw  : std_logic_vector(8-1 downto 0);
   signal   data_in_rbusy     : std_logic;
 
+  constant INIT_data_out : std_logic_vector(8-1 downto 0) :=
+             "00000000" -- value
+           ;
   signal   data_out_wcs       : std_logic;
   signal   data_out_we        : std_logic;
   signal   data_out_wdata     : std_logic_vector(8-1 downto 0);
@@ -119,6 +131,8 @@ begin  -- architecture rtl
                sig_rbusy when sig_re = '1' else
                '0';
 
+  gen_data: if (True)
+  generate
   --==================================
   -- Register    : data
   -- Description : data - with data_oe mask apply
@@ -135,47 +149,62 @@ begin  -- architecture rtl
   --==================================
 
 
-  data_rcs     <= '1' when     (sig_raddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(0,GPIO_ADDR_WIDTH))) else '0';
-  data_re      <= sig_rcs and sig_re and data_rcs;
-  data_rdata   <= (
-    0 => data_rdata_sw(0), -- value(0)
-    1 => data_rdata_sw(1), -- value(1)
-    2 => data_rdata_sw(2), -- value(2)
-    3 => data_rdata_sw(3), -- value(3)
-    4 => data_rdata_sw(4), -- value(4)
-    5 => data_rdata_sw(5), -- value(5)
-    6 => data_rdata_sw(6), -- value(6)
-    7 => data_rdata_sw(7), -- value(7)
-    others => '0');
+    data_rcs     <= '1' when     (sig_raddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(0,GPIO_ADDR_WIDTH))) else '0';
+    data_re      <= sig_rcs and sig_re and data_rcs;
+    data_rdata   <= (
+      0 => data_rdata_sw(0), -- value(0)
+      1 => data_rdata_sw(1), -- value(1)
+      2 => data_rdata_sw(2), -- value(2)
+      3 => data_rdata_sw(3), -- value(3)
+      4 => data_rdata_sw(4), -- value(4)
+      5 => data_rdata_sw(5), -- value(5)
+      6 => data_rdata_sw(6), -- value(6)
+      7 => data_rdata_sw(7), -- value(7)
+      others => '0');
 
-  data_wcs      <= '0';
-  data_we       <= '0';
-  data_wbusy    <= '0';
-  data_wdata    <= (others=>'0');
-  data_wdata_sw <= (others=>'0');
-  data_wdata_hw(7 downto 0) <= hw2sw_i.data.value; -- value
-  sw2hw_o.data.value <= data_rdata_hw(7 downto 0); -- value
+    data_wcs      <= '0';
+    data_we       <= '0';
+    data_wbusy    <= '0';
+    data_wdata    <= (others=>'0');
+    data_wdata_sw <= (others=>'0');
+    data_wdata_hw(7 downto 0) <= hw2sw_i.data.value; -- value
+    sw2hw_o.data.value <= data_rdata_hw(7 downto 0); -- value
 
-  ins_data : entity work.csr_ext(rtl)
-    generic map
-      (WIDTH         => 8
-      )
-    port map
-      (clk_i         => clk_i
-      ,arst_b_i      => arst_b_i
-      ,sw_wd_i       => data_wdata_sw
-      ,sw_rd_o       => data_rdata_sw
-      ,sw_we_i       => data_we
-      ,sw_re_i       => data_re
-      ,sw_rbusy_o    => data_rbusy
-      ,sw_wbusy_o    => data_wbusy
-      ,hw_wd_i       => data_wdata_hw
-      ,hw_rd_o       => data_rdata_hw
-      ,hw_we_i       => hw2sw_i.data.we
-      ,hw_sw_re_o    => sw2hw_o.data.re
-      ,hw_sw_we_o      => open
-      );
+    ins_data : entity work.csr_ext(rtl)
+      generic map
+        (WIDTH         => 8
+        )
+      port map
+        (clk_i         => clk_i
+        ,arst_b_i      => arst_b_i
+        ,sw_wd_i       => data_wdata_sw
+        ,sw_rd_o       => data_rdata_sw
+        ,sw_we_i       => data_we
+        ,sw_re_i       => data_re
+        ,sw_rbusy_o    => data_rbusy
+        ,sw_wbusy_o    => data_wbusy
+        ,hw_wd_i       => data_wdata_hw
+        ,hw_rd_o       => data_rdata_hw
+        ,hw_we_i       => hw2sw_i.data.we
+        ,hw_sw_re_o    => sw2hw_o.data.re
+        ,hw_sw_we_o      => open
+        );
 
+  end generate gen_data;
+
+  gen_data_b: if not (True)
+  generate
+    data_rcs     <= '0';
+    data_rbusy   <= '0';
+    data_rdata   <= (others => '0');
+    data_wcs      <= '0';
+    data_wbusy    <= '0';
+    sw2hw_o.data.value <= "00000000";
+    sw2hw_o.data.re <= '0';
+  end generate gen_data_b;
+
+  gen_data_oe: if (True)
+  generate
   --==================================
   -- Register    : data_oe
   -- Description : GPIO Direction
@@ -192,47 +221,63 @@ begin  -- architecture rtl
   --==================================
 
 
-  data_oe_rcs     <= '1' when     (sig_raddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(1,GPIO_ADDR_WIDTH))) else '0';
-  data_oe_re      <= sig_rcs and sig_re and data_oe_rcs;
-  data_oe_rdata   <= (
-    0 => data_oe_rdata_sw(0), -- value(0)
-    1 => data_oe_rdata_sw(1), -- value(1)
-    2 => data_oe_rdata_sw(2), -- value(2)
-    3 => data_oe_rdata_sw(3), -- value(3)
-    4 => data_oe_rdata_sw(4), -- value(4)
-    5 => data_oe_rdata_sw(5), -- value(5)
-    6 => data_oe_rdata_sw(6), -- value(6)
-    7 => data_oe_rdata_sw(7), -- value(7)
-    others => '0');
+    data_oe_rcs     <= '1' when     (sig_raddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(1,GPIO_ADDR_WIDTH))) else '0';
+    data_oe_re      <= sig_rcs and sig_re and data_oe_rcs;
+    data_oe_rdata   <= (
+      0 => data_oe_rdata_sw(0), -- value(0)
+      1 => data_oe_rdata_sw(1), -- value(1)
+      2 => data_oe_rdata_sw(2), -- value(2)
+      3 => data_oe_rdata_sw(3), -- value(3)
+      4 => data_oe_rdata_sw(4), -- value(4)
+      5 => data_oe_rdata_sw(5), -- value(5)
+      6 => data_oe_rdata_sw(6), -- value(6)
+      7 => data_oe_rdata_sw(7), -- value(7)
+      others => '0');
 
-  data_oe_wcs     <= '1' when     (sig_waddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(1,GPIO_ADDR_WIDTH))) else '0';
-  data_oe_we      <= sig_wcs and sig_we and data_oe_wcs;
-  data_oe_wdata   <= sig_wdata;
-  data_oe_wdata_sw(7 downto 0) <= data_oe_wdata(7 downto 0); -- value
-  sw2hw_o.data_oe.value <= data_oe_rdata_hw(7 downto 0); -- value
+    data_oe_wcs     <= '1' when       (sig_waddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(1,GPIO_ADDR_WIDTH)))   else '0';
+    data_oe_we      <= sig_wcs and sig_we and data_oe_wcs;
+    data_oe_wdata   <= sig_wdata;
+    data_oe_wdata_sw(7 downto 0) <= data_oe_wdata(7 downto 0); -- value
+    sw2hw_o.data_oe.value <= data_oe_rdata_hw(7 downto 0); -- value
 
-  ins_data_oe : entity work.csr_reg(rtl)
-    generic map
-      (WIDTH         => 8
-      ,INIT          => "00000000" -- value
-      ,MODEL         => "rw"
-      )
-    port map
-      (clk_i         => clk_i
-      ,arst_b_i      => arst_b_i
-      ,sw_wd_i       => data_oe_wdata_sw
-      ,sw_rd_o       => data_oe_rdata_sw
-      ,sw_we_i       => data_oe_we
-      ,sw_re_i       => data_oe_re
-      ,sw_rbusy_o    => data_oe_rbusy
-      ,sw_wbusy_o    => data_oe_wbusy
-      ,hw_wd_i       => (others => '0')
-      ,hw_rd_o       => data_oe_rdata_hw
-      ,hw_we_i       => '0'
-      ,hw_sw_re_o    => sw2hw_o.data_oe.re
-      ,hw_sw_we_o    => sw2hw_o.data_oe.we
-      );
+    ins_data_oe : entity work.csr_reg(rtl)
+      generic map
+        (WIDTH         => 8
+        ,INIT          => INIT_data_oe
+        ,MODEL         => "rw"
+        )
+      port map
+        (clk_i         => clk_i
+        ,arst_b_i      => arst_b_i
+        ,sw_wd_i       => data_oe_wdata_sw
+        ,sw_rd_o       => data_oe_rdata_sw
+        ,sw_we_i       => data_oe_we
+        ,sw_re_i       => data_oe_re
+        ,sw_rbusy_o    => data_oe_rbusy
+        ,sw_wbusy_o    => data_oe_wbusy
+        ,hw_wd_i       => (others => '0')
+        ,hw_rd_o       => data_oe_rdata_hw
+        ,hw_we_i       => '0'
+        ,hw_sw_re_o    => sw2hw_o.data_oe.re
+        ,hw_sw_we_o    => sw2hw_o.data_oe.we
+        );
 
+  end generate gen_data_oe;
+
+  gen_data_oe_b: if not (True)
+  generate
+    data_oe_rcs     <= '0';
+    data_oe_rbusy   <= '0';
+    data_oe_rdata   <= (others => '0');
+    data_oe_wcs      <= '0';
+    data_oe_wbusy    <= '0';
+    sw2hw_o.data_oe.value <= "00000000";
+    sw2hw_o.data_oe.re <= '0';
+    sw2hw_o.data_oe.we <= '0';
+  end generate gen_data_oe_b;
+
+  gen_data_in: if (True)
+  generate
   --==================================
   -- Register    : data_in
   -- Description : GPIO Input
@@ -249,49 +294,64 @@ begin  -- architecture rtl
   --==================================
 
 
-  data_in_rcs     <= '1' when     (sig_raddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(2,GPIO_ADDR_WIDTH))) else '0';
-  data_in_re      <= sig_rcs and sig_re and data_in_rcs;
-  data_in_rdata   <= (
-    0 => data_in_rdata_sw(0), -- value(0)
-    1 => data_in_rdata_sw(1), -- value(1)
-    2 => data_in_rdata_sw(2), -- value(2)
-    3 => data_in_rdata_sw(3), -- value(3)
-    4 => data_in_rdata_sw(4), -- value(4)
-    5 => data_in_rdata_sw(5), -- value(5)
-    6 => data_in_rdata_sw(6), -- value(6)
-    7 => data_in_rdata_sw(7), -- value(7)
-    others => '0');
+    data_in_rcs     <= '1' when     (sig_raddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(2,GPIO_ADDR_WIDTH))) else '0';
+    data_in_re      <= sig_rcs and sig_re and data_in_rcs;
+    data_in_rdata   <= (
+      0 => data_in_rdata_sw(0), -- value(0)
+      1 => data_in_rdata_sw(1), -- value(1)
+      2 => data_in_rdata_sw(2), -- value(2)
+      3 => data_in_rdata_sw(3), -- value(3)
+      4 => data_in_rdata_sw(4), -- value(4)
+      5 => data_in_rdata_sw(5), -- value(5)
+      6 => data_in_rdata_sw(6), -- value(6)
+      7 => data_in_rdata_sw(7), -- value(7)
+      others => '0');
 
-  data_in_wcs      <= '0';
-  data_in_we       <= '0';
-  data_in_wbusy    <= '0';
-  data_in_wdata    <= (others=>'0');
-  data_in_wdata_sw <= (others=>'0');
-  data_in_wdata_hw(7 downto 0) <= hw2sw_i.data_in.value; -- value
-  sw2hw_o.data_in.value <= data_in_rdata_hw(7 downto 0); -- value
+    data_in_wcs      <= '0';
+    data_in_we       <= '0';
+    data_in_wbusy    <= '0';
+    data_in_wdata    <= (others=>'0');
+    data_in_wdata_sw <= (others=>'0');
+    data_in_wdata_hw(7 downto 0) <= hw2sw_i.data_in.value; -- value
+    sw2hw_o.data_in.value <= data_in_rdata_hw(7 downto 0); -- value
 
-  ins_data_in : entity work.csr_reg(rtl)
-    generic map
-      (WIDTH         => 8
-      ,INIT          => "00000000" -- value
-      ,MODEL         => "ro"
-      )
-    port map
-      (clk_i         => clk_i
-      ,arst_b_i      => arst_b_i
-      ,sw_wd_i       => data_in_wdata_sw
-      ,sw_rd_o       => data_in_rdata_sw
-      ,sw_we_i       => data_in_we
-      ,sw_re_i       => data_in_re
-      ,sw_rbusy_o    => data_in_rbusy
-      ,sw_wbusy_o    => data_in_wbusy
-      ,hw_wd_i       => data_in_wdata_hw
-      ,hw_rd_o       => data_in_rdata_hw
-      ,hw_we_i       => hw2sw_i.data_in.we
-      ,hw_sw_re_o    => sw2hw_o.data_in.re
-      ,hw_sw_we_o      => open
-      );
+    ins_data_in : entity work.csr_reg(rtl)
+      generic map
+        (WIDTH         => 8
+        ,INIT          => INIT_data_in
+        ,MODEL         => "ro"
+        )
+      port map
+        (clk_i         => clk_i
+        ,arst_b_i      => arst_b_i
+        ,sw_wd_i       => data_in_wdata_sw
+        ,sw_rd_o       => data_in_rdata_sw
+        ,sw_we_i       => data_in_we
+        ,sw_re_i       => data_in_re
+        ,sw_rbusy_o    => data_in_rbusy
+        ,sw_wbusy_o    => data_in_wbusy
+        ,hw_wd_i       => data_in_wdata_hw
+        ,hw_rd_o       => data_in_rdata_hw
+        ,hw_we_i       => hw2sw_i.data_in.we
+        ,hw_sw_re_o    => sw2hw_o.data_in.re
+        ,hw_sw_we_o      => open
+        );
 
+  end generate gen_data_in;
+
+  gen_data_in_b: if not (True)
+  generate
+    data_in_rcs     <= '0';
+    data_in_rbusy   <= '0';
+    data_in_rdata   <= (others => '0');
+    data_in_wcs      <= '0';
+    data_in_wbusy    <= '0';
+    sw2hw_o.data_in.value <= "00000000";
+    sw2hw_o.data_in.re <= '0';
+  end generate gen_data_in_b;
+
+  gen_data_out: if (True)
+  generate
   --==================================
   -- Register    : data_out
   -- Description : GPIO Output
@@ -308,46 +368,60 @@ begin  -- architecture rtl
   --==================================
 
 
-  data_out_rcs     <= '1' when     (sig_raddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(3,GPIO_ADDR_WIDTH))) else '0';
-  data_out_re      <= sig_rcs and sig_re and data_out_rcs;
-  data_out_rdata   <= (
-    0 => data_out_rdata_sw(0), -- value(0)
-    1 => data_out_rdata_sw(1), -- value(1)
-    2 => data_out_rdata_sw(2), -- value(2)
-    3 => data_out_rdata_sw(3), -- value(3)
-    4 => data_out_rdata_sw(4), -- value(4)
-    5 => data_out_rdata_sw(5), -- value(5)
-    6 => data_out_rdata_sw(6), -- value(6)
-    7 => data_out_rdata_sw(7), -- value(7)
-    others => '0');
+    data_out_rcs     <= '1' when     (sig_raddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(3,GPIO_ADDR_WIDTH))) else '0';
+    data_out_re      <= sig_rcs and sig_re and data_out_rcs;
+    data_out_rdata   <= (
+      0 => data_out_rdata_sw(0), -- value(0)
+      1 => data_out_rdata_sw(1), -- value(1)
+      2 => data_out_rdata_sw(2), -- value(2)
+      3 => data_out_rdata_sw(3), -- value(3)
+      4 => data_out_rdata_sw(4), -- value(4)
+      5 => data_out_rdata_sw(5), -- value(5)
+      6 => data_out_rdata_sw(6), -- value(6)
+      7 => data_out_rdata_sw(7), -- value(7)
+      others => '0');
 
-  data_out_wcs     <= '1' when     (sig_waddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(0,GPIO_ADDR_WIDTH))) or (sig_waddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(3,GPIO_ADDR_WIDTH))) else '0';
-  data_out_we      <= sig_wcs and sig_we and data_out_wcs;
-  data_out_wdata   <= sig_wdata;
-  data_out_wdata_sw(7 downto 0) <= data_out_wdata(7 downto 0); -- value
-  sw2hw_o.data_out.value <= data_out_rdata_hw(7 downto 0); -- value
+    data_out_wcs     <= '1' when       (sig_waddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(0,GPIO_ADDR_WIDTH)))   or (sig_waddr(GPIO_ADDR_WIDTH-1 downto 0) = std_logic_vector(to_unsigned(3,GPIO_ADDR_WIDTH)))   else '0';
+    data_out_we      <= sig_wcs and sig_we and data_out_wcs;
+    data_out_wdata   <= sig_wdata;
+    data_out_wdata_sw(7 downto 0) <= data_out_wdata(7 downto 0); -- value
+    sw2hw_o.data_out.value <= data_out_rdata_hw(7 downto 0); -- value
 
-  ins_data_out : entity work.csr_reg(rtl)
-    generic map
-      (WIDTH         => 8
-      ,INIT          => "00000000" -- value
-      ,MODEL         => "rw"
-      )
-    port map
-      (clk_i         => clk_i
-      ,arst_b_i      => arst_b_i
-      ,sw_wd_i       => data_out_wdata_sw
-      ,sw_rd_o       => data_out_rdata_sw
-      ,sw_we_i       => data_out_we
-      ,sw_re_i       => data_out_re
-      ,sw_rbusy_o    => data_out_rbusy
-      ,sw_wbusy_o    => data_out_wbusy
-      ,hw_wd_i       => (others => '0')
-      ,hw_rd_o       => data_out_rdata_hw
-      ,hw_we_i       => '0'
-      ,hw_sw_re_o    => sw2hw_o.data_out.re
-      ,hw_sw_we_o    => sw2hw_o.data_out.we
-      );
+    ins_data_out : entity work.csr_reg(rtl)
+      generic map
+        (WIDTH         => 8
+        ,INIT          => INIT_data_out
+        ,MODEL         => "rw"
+        )
+      port map
+        (clk_i         => clk_i
+        ,arst_b_i      => arst_b_i
+        ,sw_wd_i       => data_out_wdata_sw
+        ,sw_rd_o       => data_out_rdata_sw
+        ,sw_we_i       => data_out_we
+        ,sw_re_i       => data_out_re
+        ,sw_rbusy_o    => data_out_rbusy
+        ,sw_wbusy_o    => data_out_wbusy
+        ,hw_wd_i       => (others => '0')
+        ,hw_rd_o       => data_out_rdata_hw
+        ,hw_we_i       => '0'
+        ,hw_sw_re_o    => sw2hw_o.data_out.re
+        ,hw_sw_we_o    => sw2hw_o.data_out.we
+        );
+
+  end generate gen_data_out;
+
+  gen_data_out_b: if not (True)
+  generate
+    data_out_rcs     <= '0';
+    data_out_rbusy   <= '0';
+    data_out_rdata   <= (others => '0');
+    data_out_wcs      <= '0';
+    data_out_wbusy    <= '0';
+    sw2hw_o.data_out.value <= "00000000";
+    sw2hw_o.data_out.re <= '0';
+    sw2hw_o.data_out.we <= '0';
+  end generate gen_data_out_b;
 
   sig_wbusy <= 
     data_wbusy when data_wcs = '1' else
