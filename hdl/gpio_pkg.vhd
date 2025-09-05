7a7896454b44cfa91cfcc52a0bc26319df73eb3d
deuskane <deuskane@gmail.com>
2025-09-05 09:16:43 +0200

    Add package

---
+++ hdl/gpio_pkg.vhd
@@ -0,0 +1,95 @@
+library IEEE;
+use     IEEE.STD_LOGIC_1164.ALL;
+use     IEEE.NUMERIC_STD.ALL;
+
+package gpio_pkg is
+-- [COMPONENT_INSERT][BEGIN]
+component GPIO is
+  generic(
+    NB_IO            : natural:=8;       -- Number of IO. Must be <= SIZE_DATA
+    IT_ENABLE        : boolean:=false    -- GPIO can generate interruption
+    );
+  port   (
+    clk_i            : in    std_logic;
+    cke_i            : in    std_logic;
+    arstn_i          : in    std_logic; -- asynchronous reset
+
+    -- To/From IO
+    data_i           : in    std_logic_vector (NB_IO-1     downto 0);
+    data_o           : out   std_logic_vector (NB_IO-1     downto 0);
+    data_oe_o        : out   std_logic_vector (NB_IO-1     downto 0);
+    
+    -- To/From IT Ctrl
+    interrupt_o      : out   std_logic;
+    interrupt_ack_i  : in    std_logic;
+
+    sw2hw_i          : in    GPIO_sw2hw_t;
+    hw2sw_o          : out   GPIO_hw2sw_t
+
+    );
+end component GPIO;
+
+component GPIO_v1 is
+  generic(
+    SIZE_ADDR        : natural:=2;       -- Bus Address Width
+    SIZE_DATA        : natural:=8;       -- Bus Data    Width
+    NB_IO            : natural:=8;       -- Number of IO. Must be <= SIZE_DATA
+    DATA_OE_INIT     : std_logic_vector; -- Direction of the IO after a reset
+    DATA_OE_FORCE    : std_logic_vector; -- Can change the direction of the IO
+    IT_ENABLE        : boolean:=false    -- GPIO can generate interruption
+    );
+  port   (
+    clk_i            : in    std_logic;
+    cke_i            : in    std_logic;
+    arstn_i          : in    std_logic; -- asynchronous reset
+
+    -- To IP
+    cs_i             : in    std_logic;
+    re_i             : in    std_logic;
+    we_i             : in    std_logic;
+    addr_i           : in    std_logic_vector (SIZE_ADDR-1 downto 0);
+    wdata_i          : in    std_logic_vector (SIZE_DATA-1 downto 0);
+    rdata_o          : out   std_logic_vector (SIZE_DATA-1 downto 0);
+    busy_o           : out   std_logic;
+
+    -- To/From IO
+    data_i           : in    std_logic_vector (NB_IO-1     downto 0);
+    data_o           : out   std_logic_vector (NB_IO-1     downto 0);
+    data_oe_o        : out   std_logic_vector (NB_IO-1     downto 0);
+    
+    -- To/From IT Ctrl
+    interrupt_o      : out   std_logic;
+    interrupt_ack_i  : in    std_logic
+    );
+end component GPIO_v1;
+
+component pbi_GPIO is
+  generic(
+    NB_IO            : natural:=8;     -- Number of IO. Must be <= SIZE_DATA
+    DATA_OE_INIT     : std_logic_vector; -- Direction of the IO after a reset
+    IT_ENABLE        : boolean:=false    -- GPIO can generate interruption
+    );
+  port   (
+    clk_i            : in    std_logic;
+    cke_i            : in    std_logic;
+    arstn_i          : in    std_logic; -- asynchronous reset
+
+    -- Bus
+    pbi_ini_i        : in    pbi_ini_t;
+    pbi_tgt_o        : out   pbi_tgt_t;
+    
+    -- To/From IO
+    data_i           : in    std_logic_vector (NB_IO-1     downto 0);
+    data_o           : out   std_logic_vector (NB_IO-1     downto 0);
+    data_oe_o        : out   std_logic_vector (NB_IO-1     downto 0);
+
+    -- To/From IT Ctrl
+    interrupt_o      : out   std_logic;
+    interrupt_ack_i  : in    std_logic
+    );
+
+end component pbi_GPIO;
+
+-- [COMPONENT_INSERT][END]
+
+end gpio_pkg;
